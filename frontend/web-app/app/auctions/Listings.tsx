bf761692ee9446db67b559d9bdf10a9208292d9d
elisha1995 <elishasamuel01@gmail.com>
2024-05-09 08:11:50 +0000

    Adding pagination to the listings

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,23 +1,61 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
+import { Auction, PagedResult } from "@/types";
+import AppPagination from "../components/AppPagination";
+import { getData } from "../actions/auctionActions";
+import Filters from "./Filters";
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { shallow } from "zustand/shallow";
+import qs from "query-string";
 
-async function getData() {
-  const res = await fetch("http://localhost:6001/search?pageSize=10");
 
-  if (!res.ok) throw new Error("Failed to fetch data");
+export default function Listings() {
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(
+    (state) => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy,
+      seller: state.seller,
+      winner: state.winner,
+    }),
+    shallow
+  );
 
-  return res.json();
-}
+   const setParams = useParamsStore((state) => state.setParams);
+   const url = qs.stringifyUrl({ url: "", query: params });
 
-export default async function Listings() {
-  const data = await getData();
+   function setPageNumber(pageNumber: number) {
+     setParams({ pageNumber });
+   }
+
+  useEffect(() => {
+    getData(url).then((data) => {
+      setData(data);
+    });
+  }, [url]);
+
+  if (!data) return <h3>Loading...</h3>;
 
   return (
-    <div className="grid grid-cols-4 gap-6">
-      {data &&
-        data.results.map((auction: any) => (
+    <>
+      <Filters />
+      <div className="grid grid-cols-4 gap-6">
+        {data.results.map((auction) => (
           <AuctionCard auction={auction} key={auction.id} />
         ))}
-    </div>
+      </div>
+      <div className="flex justify-center mt-4">
+        <AppPagination
+          pageChanged={setPageNumber}
+          currentPage={params.pageNumber}
+          pageCount={data.pageCount}
+        />
+      </div>
+    </>
   );
 }
